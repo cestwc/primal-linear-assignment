3d4677bc787582782add41eb2bda7f16be11f609
cestwc <80936226+cestwc@users.noreply.github.com>
2025-08-09 13:50:40 +0800

    update cuda solver

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-import os
-# os.environ["TORCH_CUDA_ARCH_LIST"] = "8.6"  # Or "native" if supported
-os.environ["TORCH_CUDA_ARCH_LIST"] = "7.5;8.0;8.6;8.9;9.0+PTX"
 readme = open('README.md').read()
 
 VERSION = '0.0.7'
 
-requirements = []
+requirements = [
+]
+
 
 setup(
     # Metadata
@@ -24,35 +22,18 @@ setup(
     # Package info
     packages=find_packages(exclude=('*test*',)),
 
-    # CUDA extension
-    ext_modules=[
-        CUDAExtension(
-            'balinski_and_gomory.cuda_solver',  # module name (import solve)
-            sources=[
-                'balinski_and_gomory/solve.cpp',
-                'balinski_and_gomory/balinski-and-gomory-cuda/src/solver.cu',
-                # 'balinski_and_gomory/balinski-and-gomory-cuda/src/solver.h'
-            ],
-            # include_dirs=[
-            #     "balinski_and_gomory/balinski-and-gomory-cuda/src",  # ✅ where solver.h lives
-            # ],
-            extra_compile_args={
-                'cxx': ['-O3'],
-                'nvcc': ['-O3'],# '-arch=sm_86'],  # adjust as needed
-            }
-        )
-    ],
-    cmdclass={'build_ext': BuildExtension},
-
-    zip_safe=False,
+    #
+    zip_safe=True,
     install_requires=requirements,
-    include_package_data=True,
-    package_data={
-        "balinski_and_gomory.hylac_shortcut": ["libhylac.so"],
-    },
 
+    # Classifiers
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
+
+    include_package_data=True,
+    package_data={
+        "balinski_and_gomory.hylac_shortcut": ["libhylac.so"],
+    },
 )
